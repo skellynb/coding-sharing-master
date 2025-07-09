e00ce2109e6deacabdea30a29b271f0c7d7e7e5f
skellynb <bisisina@gmail.com>
2025-07-09 12:50:24 +0100

    created Get endpoints and tested

---
+++ note-code/src/app/api/snippets/[id]/route.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "../../../../../generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+
+
+export async function GET (
+  req: NextRequest,
+   { params } : { params: {id: string} }
+) {
+  const {id} = params;
+
+  try {
+    const snippet = await prisma.snippet.findUnique({
+      where: {id}
+    });
+
+    if (!snippet) {
+      return NextResponse.json({ error: 'Snippet not found' }, { status: 404 });
+
+    }
+        return NextResponse.json({
+          code: snippet.code,
+          language: snippet.language,
+          theme: snippet.theme,
+          createdAt: snippet.createdAt
+        });
+      
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({error: 'server error'}, {status:500});
+      }
+  
+}
